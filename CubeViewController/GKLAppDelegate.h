b4f87f3198a6dd14fa6a322f6c4f8ee647423e89
Joe Pintozzi <joe@gokartlabs.com>
2012-11-28 13:14:18 -0600

    Initial Commit

---
+++ CubeViewController/GKLAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GKLAppDelegate.h
+//  CubeViewController
+//
+//  Created by Joseph Pintozzi on 11/28/12.
+//  Copyright (c) 2012 GoKart Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class GKLViewController;
+
+@interface GKLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) GKLViewController *viewController;
+
+@end
---
+++ CubeViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CubeViewController
+//
+//  Created by Joseph Pintozzi on 11/28/12.
+//  Copyright (c) 2012 GoKart Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GKLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GKLAppDelegate class]));
+    }
+}
